3c463b831c336d73ec8cb0412a50ab104a756036
Quang Trung <handoilaanmay@gmail.com>
2021-04-27 22:33:12 +0700

    firstcommit

--- src/main/java/com/controller/PostController.java
+++ src/main/java/com/controller/PostController.java
@@ -27,7 +27,7 @@ public class PostController {
 	@Autowired
 	PostServiceImpl postServiceImpl;
 
-	@RequestMapping(path = "/addpost", method = RequestMethod.GET)
+	@RequestMapping(path = "/addpost123", method = RequestMethod.GET)
 	public String createpost(Model model, HttpSession session) {
 		User user = null;
 		user = (User) session.getAttribute("User");
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module Testgithub {
+	exports com.modal;
+	exports com.controller;
+	exports com.repositories;
+	exports com.services;
+	exports com.entities;
+	exports com.untils;
+
+	requires servlet.api;
+}
